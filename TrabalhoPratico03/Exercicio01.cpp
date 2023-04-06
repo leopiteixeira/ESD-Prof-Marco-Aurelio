dc99d52da30c559e9b9c81b35d5c6e5faab76d81
leopiteixeira <leonardo.p.t.ps4123@gmail.com>
2023-04-05 22:21:23 -0300

    trabalho pratico 02

---
+++ TrabalhoPratico03/Exercicio01.cpp
@@ -0,0 +1,187 @@
+#include <iostream>
+#include <locale.h>
+#include <string>
+#include<stdio.h>
+#include<time.h>
+#include <cstdlib>
+using namespace std;
+
+class Data
+{
+	private:
+		int dia;
+		int mes;
+		int ano;
+    public:
+    
+    	Data(int dia, int mes, int ano) {
+			this->dia = dia;
+            this->mes = mes;
+			this->ano = ano;			
+		}
+    	Data() {
+			this->dia = 0;
+            this->mes = 0;
+			this->ano = 0;			
+		}
+		void setDia(int dia) {
+			this->dia = dia;
+		}
+		void setMes(int mes) {
+			this->mes = mes;
+		}
+		void setAno(int ano) {
+			this->ano = ano;
+		}
+		int getDia() {
+			return this->dia;
+		}
+		int getMes() {
+			return this->mes;
+		}
+		int getAno() {
+			return this->ano;
+		}
+		string getData()
+        {
+            return to_string(this->dia) + "/" +
+                   to_string(this->mes) + "/" +
+           		   to_string(this->ano);	  
+        }
+        
+        Data* dia_seguinte()
+		{
+			Data *d1;
+			int diasNoMes[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+			d1 = new Data(this->dia, this->mes, this->ano);
+			if (d1->ano%4==0)
+			{
+				diasNoMes[1]++;
+			}
+			d1->dia++;
+			if(d1->dia > diasNoMes[d1->mes-1])
+			{
+				d1->dia = 1;
+				if (++d1->mes > 12)
+				{
+					d1->mes = 1;
+					d1->ano++;
+				}
+			}
+			return d1;
+		}
+		 	
+};
+
+class Contato
+{
+	private:
+		string email;
+		string nome;
+		string telefone;
+		Data *dtnasc;
+    public:
+    	Contato(string nome, string email, string telefone, Data *dtnasc) {
+			this->nome = nome;
+            this->email = email;
+			this->telefone = telefone;
+			this->dtnasc = dtnasc;		
+		}
+    	Contato() {
+			this->nome = "";
+            this->email = "";
+			this->telefone = "";
+			this->dtnasc = new Data();			
+		}
+		void setNome(string nome) {
+			this->nome = nome;
+		}
+		void setEmail(string email) {
+			this->email = email;
+		}
+		void setTelefone(string telefone) {
+			this->telefone = telefone;
+		}
+		void setData(int dia, int mes, int ano) {
+			this->dtnasc = new Data(dia,mes,ano);
+		}
+		string getNome() {
+			return this->nome;
+		}
+		string getEmail() {
+			return this->email;
+		}
+		string getTelefone() {
+			return this->telefone;
+		}
+		string getData()
+        {
+            return to_string(this->dtnasc->getDia()) + "/" +
+                   to_string(this->dtnasc->getMes()) + "/" +
+           		   to_string(this->dtnasc->getAno());	  
+        }
+        
+        int idade(int anoAtual){
+			return anoAtual - (this->dtnasc->getAno());
+		}
+        
+		 	
+};
+
+int main(int argc, char** argv)
+{
+	string n, e, t;
+	int dia, mes, ano, ind;
+	ind = 0;
+	
+	time_t mytime;
+    mytime = time(NULL);
+    struct tm tm = *localtime(&mytime);
+    int anoAtual = tm.tm_year + 1900;
+    
+    Data *d;
+    
+    Contato* contatos[5];
+	
+	for(int i = 1; i <= 5; i++){
+		cout << "Digite o nome do contato " << i << ": ";
+		cin >> n;
+		cout << "Digite o email do contato " << i << ": ";
+		cin >> e;
+		cout << "Digite o telefone do contato " << i << ": ";
+		cin >> t;
+		cout <<"Dia do nascimento do contato " << i << ": ";
+		cin >> dia;
+		cout <<"Mes do nascimento do contato " << i << ": ";
+		cin >> mes;
+		cout <<"Ano do nascimento do contato " << i << ": ";
+		cin >> ano;
+		
+		d = new Data(dia, mes, ano);
+		
+		contatos[i - 1] = new Contato(n, e, t, d);
+		
+		system("cls");
+		
+		cout << "Contato Criado!" << endl;
+	}
+	
+	while(ind < 6){
+		cout << "quer que exiba qual contato(6 para sair):";
+		cin >> ind;
+		
+		cout << "Nome: " << contatos[ind - 1]->getNome() << endl;
+		cout << "Email: " << contatos[ind - 1]->getEmail() << endl;
+		cout << "Telefone: " << contatos[ind - 1]->getTelefone() << endl;
+		cout << "Data de nascimento: " << contatos[ind - 1]->getData() << endl;
+		cout << "Idade: " << contatos[ind - 1]->idade(anoAtual) << endl;
+		
+		system("PAUSE");
+		system("cls");
+		
+	}
+	
+	
+	
+	return 0;
+}
